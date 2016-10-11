90a0dc1dbb711bcfa4ea6b7e2327e07237e3073b
zhendi <zhendi.wang@gmail.com>
2016-10-11 09:46:55 +0800

    use icon to fix back button too small

--- app/navigation/router.js
+++ app/navigation/router.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import NavigationBar from 'react-native-navbar'
 import TabNavigator from './tab_navigator'
 import tabStore from '../stores/tab_store'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default {
   renderScene (route, navigator) {
@@ -11,13 +12,20 @@ export default {
     };
 
     let leftButtonConfig = { title: '' };
-    if(! route.hideBackButton) {
-      leftButtonConfig = {
-        title: ' <',
-        handler: ()=>
-          navigator.pop()
-      }
+
+    const renderLeftButton = () => {
+      return (
+        <TouchableOpacity style={{flexDirection: 'row', paddingLeft: 10, alignItems: 'center'}} onPress={ ()=>{ navigator.pop() }}>
+          <Icon name="angle-left" size={25} />
+          <Text style={{marginLeft: 5}}>返回</Text>
+        </TouchableOpacity>
+      )
     }
+
+    if(! route.hideBackButton) {
+      leftButtonConfig = renderLeftButton();
+    }
+
     let view = <route.component {...route.store} navigator={navigator}/>;
 
     if(route.showTabBar){
